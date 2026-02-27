28c51daa9174fcf612652d3c28d76eabee1db942
Joe <joe@joeebner.com>
2026-02-27 14:06:16 +0700

    Add all files including index.js with backdrops route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import PhotoBooks from './PhotoBooks';
 import AreasServed from './AreasServed';
+import Backdrops from './Backdrops';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +15,7 @@ root.render(
         <Route path="/" element={<App />} />
         <Route path="/photo-books" element={<PhotoBooks />} />
         <Route path="/areas-served" element={<AreasServed />} />
+        <Route path="/backdrops" element={<Backdrops />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
